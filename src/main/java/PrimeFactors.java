b88b1ba2f8ea94e3e2c5dd1e50794e12c9cdec02
Yifei Li <yfli@CNyfli.local>
2017-08-02 20:53:51 +0800

    fourth commit

---
+++ src/main/java/PrimeFactors.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeFactors {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        String result = generate(n);
+
+        System.out.printf(result.substring(1));
+    }
+
+    public static String generate(int n){
+        String result = "";
+        for(int i = 2;i <= n/2;i++){
+            if(n % i == 0) {
+                int j = 2;
+                for (j = 2; j <= i / 2; j++) {
+                    if(i % j == 0)
+                        break;
+                }
+                if(j == i/2+1)
+                    result += "," + i;
+            }
+        }
+        return result;
+    }
+}
